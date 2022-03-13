085a08db6ad9eb53eb9e0a97080b4adcf59a65a3
Kazi Islam <kazichaska@gmail.com>
2022-03-13 17:07:01 -0500

    added new package for welcome message

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const greet = require('greet-by-time');
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+const moment = require('moment');
+var today = moment().format("[Today is: ] dddd");
+
+function getGreeting(username) {
+    const hour = new Date().getHours();
+    return greet(username, hour);
+}
+
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // use the ID from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'contents',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // serialize data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', {
+                posts,
+                loggedIn: req.session.loggedIn,
+                // loggedIn: true
+                username: req.session.username,
+                greeting: getGreeting(req.session.username),
+                moment: today,
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/edit/:id', (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'contents',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (dbPostData) {
+                const post = dbPostData.get({ plain: true });
+                res.render('edit-post', {
+                    post,
+                    loggedIn: req.session.loggedIn
+                    // loggedIn: true
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-// const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -24,15 +24,15 @@ const sess = {
 };
 app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 // IMPORTANT
